f738e0bddfcdbeb121a3cddedab1da8ad32310c5
Alexander Nichiporovich <a.nichiporovich@gmail.com>
2020-02-25 23:16:11 +0300

    Module 7 - Task 2 - Remove console.log.

--- js/data.js
+++ js/data.js
@@ -64,7 +64,6 @@
   }
 
   function applyFilter(filter) {
-    console.log('filtering data...');
     apartments = initialApartments.filter(function (apartment) {
       if (filter.housingType !== 'any' && apartment.offer.type !== filter.housingType) {
         return false;
@@ -73,12 +72,9 @@
       return true;
     });
 
-
     if (filter.maxItems) {
       apartments = apartments.slice(0, filter.maxItems);
     }
-
-    console.log(apartments);
   }
 
   window.data = window.data || {};
