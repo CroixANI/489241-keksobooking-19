827a324d219d6f93109ccdd5062ba06b8e790ffa
Alexander Nichiporovich <a.nichiporovich@gmail.com>
2020-02-25 23:14:29 +0300

    Module 7 - Task 2 - Implement filter by housing type.

--- js/data.js
+++ js/data.js
@@ -25,13 +25,19 @@
     }
   };
 
+  var initialApartments;
   var apartments;
 
-  function getApartments(onLoad) {
+  function getApartments() {
+    return apartments;
+  }
+
+  function loadData(onLoad) {
     window.backend.load(PINS_DATA_URL, function (data) {
       if (data instanceof Array) {
-        apartments = data;
-        onLoad(data);
+        initialApartments = data;
+        apartments = initialApartments.slice();
+        onLoad();
       } else {
         window.utils.showError('Unable to load data.');
       }
@@ -57,10 +63,30 @@
     window.backend.save(SUBMIT_URL, data, onLoad, window.utils.showError);
   }
 
+  function applyFilter(filter) {
+    console.log('filtering data...');
+    apartments = initialApartments.filter(function (apartment) {
+      if (filter.housingType !== 'any' && apartment.offer.type !== filter.housingType) {
+        return false;
+      }
+
+      return true;
+    });
+
+
+    if (filter.maxItems) {
+      apartments = apartments.slice(0, filter.maxItems);
+    }
+
+    console.log(apartments);
+  }
+
   window.data = window.data || {};
   window.data.getFeatures = getFeatures;
   window.data.getApartmentTypesData = getApartmentTypesData;
   window.data.getApartments = getApartments;
   window.data.getApartmentByIndex = getApartmentByIndex;
   window.data.submitApartment = submitApartment;
+  window.data.loadData = loadData;
+  window.data.applyFilter = applyFilter;
 })();
--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@
 
 window.page.setOnPageStateChangedListener(function (isActive) {
   if (isActive) {
-    window.map.showMap();
-    window.form.toggleDisabledState(false);
-    window.map.filter.toggleDisabledState(false);
+    window.data.loadData(function () {
+      window.form.toggleDisabledState(false);
+      window.map.filter.toggleDisabledState(false);
+      window.map.showMap();
+    });
   } else {
     window.map.hideMap();
     window.pins.removePins();
